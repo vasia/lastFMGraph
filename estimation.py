ace8fc7f5722bd5a33ff728be7ae97f2df3cdc87
vasia <vasilikikalavri@gmail.com>
2011-11-12 16:53:53 +0100

    estimation file

--- estimation.py
+++ estimation.py
@@ -2,27 +2,28 @@ import urllib2
 import random
 import sys
 
+sample = []
+degree_sample = []
+
 #reads the sample from a file
 #and creates the two lists
 #sample and degree_sample
 #filename: the file containing the samples
 #1st column is the username
 #2nd column is the node degree
-def read_samples(filename, sample, degree_sample):
+def read_samples(filename):
 	f = open(filename, 'r')
 	for line in f:
 		elements = line.split()
 		sample.append(elements[0])
 		degree_sample.append(int(elements[1]))
-	print sample
-	print degree_sample
 
 ## population estimation ##
 #sample: usernames sample
 #degree_sample: sample of node degrees
 #k: thinning parameter
-def thinning_estimation(sample,degree_sample,k):
-	pop_total = 0
+def thinning_estimation(k):
+	pop_tot = 0
 	for i in range(k):
 		total_degree = 0
 	        total_inverse_degree = 0
@@ -38,19 +39,28 @@ def thinning_estimation(sample,degree_sample,k):
 	return pop_tot/k
 
 #Calculates the collisions in the sample
-def collision_number(sample):
+def collision_number(sample_k):
 	collisions = 0
-	for i in range(len(sample)-1):
-		for j in range (i+1, len(sample)):
-			if sample[i] == sample[j]:
+	for i in range(len(sample_k)-1):
+		for j in range (i+1, len(sample_k)):
+			if sample_k[i] == sample_k[j]:
 				collisions += 1
 	return collisions
 
 if __name__ == '__main__':
 	
-	if len(sys.argv) != 2:
-		print "\nUsage: " + sys.argv[0] + " inputFile\n"
+	if len(sys.argv) != 3:
+		print "\nUsage: " + sys.argv[0] + " inputFile k\n"
+		sys.exit(0)
 
+	#initialization
 	sample = []
 	degree_sample = []
-	read_samples(sys.argv[1], sample, degree_sample)	
+	inputFile = sys.argv[1]
+	k = int(sys.argv[2])
+	#read the file
+	read_samples(inputFile)
+	#calculate the estimation
+	estim_pop = thinning_estimation(k)
+	#print results
+	print estim_pop
