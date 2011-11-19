835a019d9c80ae76d28fc54621ef1b4190cbdd71
vasia <vasilikikalavri@gmail.com>
2011-11-19 12:30:23 +0100

    fixed overflow and cleanup prints

--- estimation.py
+++ estimation.py
@@ -27,26 +27,27 @@ def read_samples(filename):
 def thinning_estimation(k):
 	pop_tot = 0
 	population = 0
+	zeros = 0
 	for i in range(k):
 		total_degree = 0
 	        total_inverse_degree = 0
 		sample_k = sample[i::k] #wow :D
-		#print sample_k
 	 	degree_k = degree_sample[i::k]
-		#print degree_k
 		collisions = collision_number(sample_k)
 		for j in range(len(degree_k)):
 			total_degree += degree_k[j]
 			total_inverse_degree += 1.0/degree_k[j]
 
-		#print "total degree = " + str(total_degree)
-		#print "total inverse degree = " + str(total_inverse_degree)
-		population = (total_degree * total_inverse_degree)/ 2*collisions
-		#print "population = " + str(population)
-		pop_tot += population
-		#print "pop_tot = " + str(pop_tot)
-	
-	return float(pop_tot)/float(k)
+		if collisions > 0:
+			population = float((float(total_degree)*float(total_inverse_degree))/(2.0*collisions))
+			pop_tot += population
+		else:
+			zeros += 1
+	if k-zeros > 0:
+		return_value = float(pop_tot)/float(k-zeros)
+	else:
+		return_value = -1
+	return return_value
 
 #Calculates the collisions in the sample
 def collision_number(sample_k):
@@ -54,7 +55,6 @@ def collision_number(sample_k):
 	for i in range(len(sample_k)-1):
 		for j in range (i+1, len(sample_k)):
 			if sample_k[i] == sample_k[j]:
-				#print "Collision found! " + str(sample_k[i])
 				collisions += 1
 	return collisions
 
@@ -71,11 +71,8 @@ if __name__ == '__main__':
 	
 	#read the file
 	read_samples(inputFile)
-	#print "sample = " + str(sample)
-	#print "degree sample = " + str(degree_sample)
-	
-	#print collision_number(sample)
 	#calculate the estimation
 	estim_pop = thinning_estimation(k)
 	#print results
 	print "Estimation = " + str(estim_pop)
+
